1379f5e0bce3b6f3d3f3d2c2b06d21ae4582d03c
museun <museun@outlook.com>
2019-06-04 19:21:02 -0400

    update to win 0.3

--- src/win.rs
+++ src/win.rs
@@ -1,39 +1,27 @@
 use self::super::Size;
-use std::ptr;
 
 /// Gets the current terminal size
 pub fn get() -> Option<Size> {
-    // http://rosettacode.org/wiki/Terminal_control/Dimensions#Windows
-    use self::kernel32::{self, GetConsoleScreenBufferInfo};
-    use self::winapi::{self, CONSOLE_SCREEN_BUFFER_INFO, HANDLE,
-                       INVALID_HANDLE_VALUE};
-    let handle: HANDLE = unsafe {
-        kernel32::CreateFileA(
-            b"CONOUT$\0".as_ptr() as *const i8,
-            winapi::GENERIC_READ | winapi::GENERIC_WRITE,
-            winapi::FILE_SHARE_WRITE,
-            ptr::null_mut(),
-            winapi::OPEN_EXISTING,
-            0,
-            ptr::null_mut(),
-        )
+    use winapi::um::{
+        handleapi::INVALID_HANDLE_VALUE, processenv::GetStdHandle,
+        winbase::STD_OUTPUT_HANDLE, wincon::GetConsoleScreenBufferInfo,
     };
+
+    let handle = unsafe { GetStdHandle(STD_OUTPUT_HANDLE) };
     if handle == INVALID_HANDLE_VALUE {
         return None;
     }
-    let info = unsafe {
-        // https://msdn.microsoft.com/en-us/library/windows/desktop/ms683171(v=vs.85).aspx
-        let mut info = ::std::mem::uninitialized();
-        if GetConsoleScreenBufferInfo(handle, &mut info) == 0 {
-            None
+
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms683171(v=vs.85).aspx
+    unsafe {
+        let mut info = std::mem::zeroed();
+        if GetConsoleScreenBufferInfo(handle, &mut info) != 0 {
+            Some(Size {
+                rows: (info.srWindow.Bottom - info.srWindow.Top + 1) as u16,
+                cols: (info.srWindow.Right - info.srWindow.Left + 1) as u16,
+            })
         } else {
-            Some(info)
+            None
         }
-    };
-    info.map(|inf| {
-        Size {
-            rows: (inf.srWindow.Bottom - inf.srWindow.Top + 1) as u16,
-            cols: (inf.srWindow.Right - inf.srWindow.Left + 1) as u16,
-        }
-    })
+    }
 }
